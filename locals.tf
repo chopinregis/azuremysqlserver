a066e35a296dcad84d5e8494b50a0b261aa038d3
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-15 19:21:54 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 locals{
-  mysql_server_name = azurerm_mysql_server.mysqlapps.name
   myregissql_app=[for f in fileset("${path.module}/config", "[^_]*.yaml") : yamldecode(file("${path.module}/config/${f}"))]
   myregissql_app_list = flatten([
     for app in local.myregissql_app : [
