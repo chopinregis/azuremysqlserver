4b720b2043213bf82058cef51d6e612691f7ea90
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-15 19:37:38 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals{
   myregissql_app=[for f in fileset("${path.module}/config", "[^_]*.yaml") : yamldecode(file("${path.module}/config/${f}"))]
   myregissql_app_list = flatten([
     for app in local.myregissql_app : [
-      for msqlapps in try(app.listofmsqlapp, []) :{
+      for mysqlapps in try(app.listofmsqlapp, []) :{
         name = mysqlapps.name
 
       }
