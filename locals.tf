f56f87958caffef7211cd35176f98b19e5781637
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-15 19:00:40 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,11 +1,11 @@
 locals{
-  linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
-  linux_app_list = flatten([
-    for app in local.linux_app : [
-      for linuxapps in try(app.listoflinuxapp, []) :{
-        name=linuxapps.name
-        os_type=linuxapps.os_type
-        sku_name=linuxapps.sku_name
+  myregissql_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
+  myregissql_app_list = flatten([
+    for app in local.myregissql_app : [
+      for msqlapps in try(app.listofmsqlapp, []) :{
+        name = mysqlapps.name
+        os_type = mysqlapps.os_type
+        sku_name = mysqlapps.sku_name
 
       }
     ]
